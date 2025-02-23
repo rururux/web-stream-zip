dbdd05912afeb3cbb161b533c1c4b04e2e0ccd84
rururux <rururux@gmail.com>
2025-02-23 23:03:51 +0900

    別のテスト追加

--- tests/stream.test.ts
+++ tests/stream.test.ts
@@ -110,4 +110,63 @@ describe("ZipStream", () => {
       await rm(outputDirPath, { force: true, recursive: true })
     }
   })
+
+  it("細切れの Stream で動作確認", async () => {
+    const outputDirPath = "./tests/assets/chunkedStream"
+    const zipFilePath = "./tests/assets/chunkedStream.zip"
+
+    try {
+      const filePaths = [
+        // Photo by BALAZS GABOR on Unsplash
+        // https://unsplash.com/@balazsgabor17
+        "image1.jpg",
+        // Photo by Colin Watts on Unsplash
+        // https://unsplash.com/@colinwatts
+        "image2.jpg",
+        // Photo by Kevin Charit on Unsplash
+        // https://unsplash.com/@kevin_charit
+        "image3.jpg",
+        "test.txt",
+        "テスト.txt"
+      ]
+      const files = await Promise.all(filePaths.map(filePath => getFile(filePath)))
+      const zipStream = new ZipStream()
+      const writeFilePromise = writeFile(zipFilePath, zipStream.readable)
+
+      for (const file of files) {
+        await file
+          .stream()
+          .pipeThrough(
+            new TransformStream({
+              transform(chunk, controller) {
+                const chunkSize = 12
+
+                for (let i = 0; i < chunk.byteLength; i += chunkSize) {
+                  const subChunk = chunk.slice(i, i + chunkSize)
+
+                  controller.enqueue(subChunk)
+                }
+              }
+            })
+          )
+          .pipeThrough(
+            new ZipEntryStream({ fileName: file.name, lastModified: file.lastModified })
+          )
+          .pipeTo(zipStream.writable, { preventClose: true })
+      }
+
+      await zipStream.writable.close()
+      await writeFilePromise
+
+      await mkdir(outputDirPath)
+      await assert.doesNotReject(exec(`tar -xf ${zipFilePath} -C ${outputDirPath}`), "cmd での解凍に失敗")
+
+      const hasDiffResults = await Promise.all(filePaths.map(filePath => hasFilesDiff(`./tests/assets/${filePath}`, `${outputDirPath}/${filePath}`)))
+
+      assert.ok(hasDiffResults.every(result => result === false), "バイナリが一致しなかった")
+    } finally {
+      await rm(zipFilePath, { force: true })
+      await rm(outputDirPath, { force: true, recursive: true })
+    }
+  })
 })
